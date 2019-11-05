3775a012e8343b9e6a055f8316a42b29d912dca8
Craig Swanson <cswanson02@me.com>
2019-11-05 13:06:04 -0600

    Created method to sign up a user.

---
+++ Gigs/Gigs/GigController.swift
@@ -0,0 +1,59 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Craig Swanson on 11/5/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+class GigController {
+    
+    var bearer: Bearer?
+    private let baseURL = URL(string: "https://lambdagigs.vapor.cloud.api")!
+    
+    
+    // MARK: Methods
+    // sign up method
+    func signUp(with user: User, completion: @escaping (Error?) -> ()) {
+        let signUpURL = baseURL.appendingPathComponent("users/signup")
+        
+        var request = URLRequest(url: signUpURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
+    // log in method
+}
