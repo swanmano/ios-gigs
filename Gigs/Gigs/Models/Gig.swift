ced2a15c749cbda0f8c071c8959e8640ffdbaa7e
Craig Swanson <cswanson02@me.com>
2019-11-07 11:13:02 -0600

    Created Gig Model

---
+++ Gigs/Gigs/Models/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Craig Swanson on 11/7/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+
+struct Gig {
+    let title: String
+    let description: String
+    let dueDate: Date
+}
